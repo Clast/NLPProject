977387a72aa491ee02a59072152db8f0af1198ef
Meghana Vellaturi <MeghanaVellaturi@cometnet-10-21-18-155.utdallas.edu>
2017-09-19 15:15:26 -0500

    added for loop to meghana.py

--- Meghana.py
+++ Meghana.py
@@ -1,1 +1,14 @@
-print('meghana')
+import urllib.request
+import bs4
+from bs4 import BeautifulSoup
+
+list_html = ["http://utdmercury.com/peer-mentors-obtain-certification/","http://utdmercury.com/freshman-project-encourages-collaboration/","http://utdmercury.com/four-utd-alumni-killed-plano-home-mass-shooting/","http://utdmercury.com/relatives-friends-remember-victims-plano-shooting/","http://utdmercury.com/jsom-offers-new-beer-class/"]
+for web in list_html:
+    #mercury_web = list_html[web]
+    page = urllib.request.urlopen(web)
+    soup = BeautifulSoup(page)
+    print(soup.prettify())
+
+
+
+
