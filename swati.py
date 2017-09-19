85f7b6d075fce9327cb70e64207c4da9d679a1b5
Swati Hirpara <shirpara1@yahoo.com>
2017-09-19 17:16:56 -0500

    foo

--- swati.py
+++ swati.py
@@ -12,11 +12,11 @@ import urllib.request
 import bs4
 from bs4 import BeautifulSoup
 
-dir = "C:\\Users\\Swati\\PycharmProjects\\sample"
-for filename in os.listdir(dir):
-    with open(dir + '/' + filename, 'r') as f:
-        sample = f.read()
-        sample = sample.lower()
+for filename in os.listdir(os.getcwd()):
+        if "d_" in filename:
+            with open(filename, 'r') as f:
+                sample = f.read()
+                sample = sample.lower()
 
 #list_html = ["http://utdmercury.com/peer-mentors-obtain-certification/","http://utdmercury.com/freshman-project-encourages-collaboration/","http://utdmercury.com/four-utd-alumni-killed-plano-home-mass-shooting/","http://utdmercury.com/relatives-friends-remember-victims-plano-shooting/","http://utdmercury.com/jsom-offers-new-beer-class/"]
 #for web in list_html:
