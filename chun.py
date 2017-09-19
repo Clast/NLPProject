ef0542f4ace6ab24ac41efa46d9c01848096a142
ChunQuanLin <chun@cagene.com>
2017-09-19 14:27:38 -0500

    test text file and chun's part of the project
    test text file and chun's part of the project

--- chun.py
+++ chun.py
@@ -1,1 +1,67 @@
-#chun's part of the project
+# Chun's part of the project
+# Write a function to extract at least 10 important terms
+# from the pages using an importance measure such as term frequency.
+# First, it’s a good idea to lower-case everything, remove stopwords and punctuation.
+# Then build a vocabulary of unique terms.
+# Create a dictionary of unique terms where the key is the token and the
+# value is the count across all documents.
+# Print the top 25-40 terms.
+def get_important_words():
+    for filename in os.getcwd():
+        if "c_" in filename:
+            file = open(filename, 'r')
+            page_text = file.read()
+            exclude = set(string.punctuation)
+            stopWords = set(stopwords.words('english'))
+            altered_text = page_text.lower()
+            altered_text = altered_text.replace("/n", " ")
+            altered_text = ''.join(ch for ch in altered_text.replace("/n", " ") if ch not in exclude)
+            tokens = nltk.word_tokenize(altered_text)
+            removed_stopwords = []
+            for word in tokens:
+                if word not in stopWords:
+                    removed_stopwords.append(word)
+            unique = set(removed_stopwords)
+            dictionary = {}
+            for word in unique:
+                if word not in dictionary:
+                    dicttmp = {word: int(1)}
+                    dictionary.update(dicttmp)
+                else:
+                    dictionary[word] = dictionary[word] + 1
+            lengths = []  # holds the 25 longest lengths
+            keys = []  # holds the keys to the 25 longest lengths
+            # gets the keys and lengths of the 25 longest lists
+            for key in dictionary:
+                if len(lengths) < 25:
+                    lengths.append(dictionary[key])
+                    keys.append(key)
+                else:
+                    smallestLength = 99999
+                    smallestIndex = 99999
+                    # gets the smallest length in the length list each time
+                    # switches out the smallest if it is smaller than dictionary at key
+                    for i in range(0, len(lengths)):
+                        if lengths[i] < smallestLength:
+                            smallestLength = lengths[i]
+                            smallestIndex = i
+                    if smallestLength < dictionary[key]:
+                        keys[smallestIndex] = key
+                        lengths[smallestIndex] = dictionary[key]
+            # sort the lengths and keys descending order
+            for i in range(0, len(lengths)):
+                maxL = lengths[i]
+                maxI = i
+                for j in range(i, len(lengths)):
+                    if maxL < lengths[j]:
+                        maxL = lengths[j]
+                        maxI = j
+                tmp = lengths[i]
+                lengths[i] = maxL
+                lengths[maxI] = tmp
+                #sorts the keys associated with the lengths
+                tmp = keys[i]
+                keys[i] = keys[maxI]
+                keys[maxI] = tmp
+            for i in keys:
+                print(i, " = ", dictionary.get(i))
