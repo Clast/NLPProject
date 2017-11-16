6da2780043b86d4ac838f6aa286551fefc8e366d
Meghana Vellaturi <MeghanaVellaturi@cometnet-10-21-8-34.utdallas.edu>
2017-11-16 14:40:17 -0600

    Modified demo code to not crash when non-key word entered

--- LoadGeneratedPickles.py
+++ LoadGeneratedPickles.py
@@ -12,23 +12,25 @@ with open('words_to_articles.pickle', 'rb') as handle:
 
 #Example
 possible_target_article = {}
-query = "police chief"
+query = "is there construction at utd"
 for word in query.split():
-    contained = words_to_articles[word]
+    if word in words_to_articles:
 
-    print(word + " is located in: ")
-    for k in contained:
-        print(k)
+        contained = words_to_articles[word]
 
-    for k in contained:
-        highestscore = 0
-        score = knowledge_base[k][2][word]
-        print ("Importance score is: " + str(score) + " in article" + k)
-        if k not in possible_target_article:
-            possible_target_article[k] = score
-        else:
-            possible_target_article[k] += score
-    print("")
+        print(word + " is located in: ")
+        for k in contained:
+            print(k)
+
+        for k in contained:
+            highestscore = 0
+            score = knowledge_base[k][2][word]
+            print ("Importance score is: " + str(score) + " in article" + k)
+            if k not in possible_target_article:
+                possible_target_article[k] = score
+            else:
+                possible_target_article[k] += score
+                print("")
 
 target_article = max(possible_target_article.items(), key=operator.itemgetter(1))[0]
 print(target_article + " has the highest importance score with " + str(possible_target_article[target_article]))
